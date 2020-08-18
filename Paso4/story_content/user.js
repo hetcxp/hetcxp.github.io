1276bcae74672740cd8c04e9ad8ff6d314d14858
hetcxp <hetcxp@gmail.com>
2020-08-17 21:10:28 -0300

    paso4

---
+++ Paso4/story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6MQbQQeXZCN":
+        Script1();
+        break;
+      case "68hcx6gBu2O":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  document.getElementById("light-box-close").style.display = "none";
+}
+
+function Script2()
+{
+  document.getElementById("light-box-close").style.display = "none";
+}
+
