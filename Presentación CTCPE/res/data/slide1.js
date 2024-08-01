2a3a34404d088530654ddccb007cdb99041f51c0
hetcxp <hetcxp@gmail.com>
2024-07-31 21:05:26 -0300

    ispringtest

---
+++ Presentación CTCPE/res/data/slide1.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{25ABF630-AB4F-4371-8C58-3E4006DC310F}'];loadHandler&&loadHandler(0, '<div id="spr0_81a47"><div id="spr1_81a47" class="kern slide"><img id="img0_81a47" src="data/img1.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_81a47" class="kern slide"><div id="vd0_81a47"></div></div></div>', '{"s":[]}');})();
